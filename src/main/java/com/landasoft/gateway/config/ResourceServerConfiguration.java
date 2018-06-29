df70bdd556806372ed1a131eb7feebacde077752
zxl <xiaolongzong@drwine.cn>
2018-06-29 14:01:27 +0800

    oauth

--- src/main/java/com/landasoft/gateway/config/ResourceServerConfiguration.java
+++ src/main/java/com/landasoft/gateway/config/ResourceServerConfiguration.java
@@ -1,29 +1,37 @@
 package com.landasoft.gateway.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 
 /**
- * @author zxl
- * @date 2018/6/28 17:11
- * @since 1.0
+ * Created by on 29.01.16.
+ *
+ * @author David Steiman
  */
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
 
-    private static final String DEMO_RESOURCE_ID = "landa-mas-corp";
-
-    @Override
-    public void configure(ResourceServerSecurityConfigurer resources) {
-        resources.resourceId(DEMO_RESOURCE_ID).stateless(true);
-    }
+    @Autowired
+    private TokenStore tokenStore;
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/landa-mas-corp/oa/user/export").authenticated();//配置访问控制，必须认证过后才可以访问
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/openapi/**").authenticated();
     }
+
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.tokenStore(tokenStore).stateless(true);
+    }
+
 }
